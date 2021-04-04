2b43535ca6298b37b9be20995176d45444ee1ebb
Cinnamon1212 <65077960+Cinnamon1212@users.noreply.github.com>
2021-04-04 22:24:34 +0100

    Update googleapi.py

--- lib/cogs/googleapi.py
+++ lib/cogs/googleapi.py
@@ -16,36 +16,6 @@ class googleapi(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name="youtube", description="Searches youtube using the Youtube V3 API", aliases=["YT", "YSearch"])
-    async def youtube(self, ctx, *, query):
-        youtube = build('youtube', 'v3', developerKey=apikey)
-        search_response = youtube.search().list(
-            q=query,
-            part='id, snippet',
-            maxResults=10
-        ).execute()
-        videos = []
-        num = 1
-        embed = Embed(title=f"Search results for {query}",
-                      colour=discord.Colour.random())
-        for search_result in search_response.get('items', []):
-            if search_result['id']['kind'] == 'youtube#video':
-                videos.append(f"\n{search_result['snippet']['title']} [Link](https://youtu.be/{search_result['id']['videoId']})\n")
-            else:
-                pass
-        for video in videos:
-            video = BeautifulSoup(video, features="html.parser")
-            embed.add_field(name=f"Result #{num}", value=f"{video}", inline=False)
-            num += 1
-        await ctx.send(embed=embed)
-
-    @youtube.error
-    async def youtube_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Please enter something to search")
-        else:
-            raise
-
     @commands.command(name="exploitdb", description="Search exploitdb", aliases=["exDB"])
     async def exploitdb(self, ctx, *, query):
         google = build('customsearch', 'v1', developerKey=apikey)
