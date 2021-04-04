134d2a0b428e3e8a855a1ba887c425728a880bc1
Cinnamon1212 <65077960+Cinnamon1212@users.noreply.github.com>
2021-04-04 12:53:33 +0100

    Update help.py

--- lib/cogs/help.py
+++ lib/cogs/help.py
@@ -1,6 +1,7 @@
 import discord, math, re
 from discord.ext import commands
 from discord import Embed
+from dpymenus import Page, PaginatedMenu
 
 def syntax(command):
     cmd_and_aliases = "|".join([str(command), *command.aliases])
@@ -19,66 +20,147 @@ class help(commands.Cog):
         self.client.remove_command('help')
 
     @commands.command(name="help", aliases=["commands", "h"], description="Displays this command")
-    async def help(self, ctx, cog="1"):
-        embed = Embed(title="Command help",
-                      description="A list of commands and how to use them!",
-                      colour=ctx.author.colour)
-        embed.set_thumbnail(url=self.client.user.avatar_url)
+    @commands.bot_has_permissions(manage_messages=True)
+    async def help(self, ctx):
+        menu = PaginatedMenu(ctx)
 
-        cogs = [c for c in self.client.cogs.keys()]
-        totalPages = math.ceil(len(cogs) / 4)
+        page1 = Page(title="Comamnd help", description="Please use ./help (cog) for help on commands within a cog!", colour=discord.Colour.random())
+        page1.add_field(name="Utility tools: ", value="Networking, website and hacking tools", inline=False)
+        page1.add_field(name="Recourses: ", value="A list of programming and hacking recourses", inline=False)
+        page1.add_field(name="AI: ", value="Commands using AI")
+        page1.add_field(name="Music: ", value="Music commands and song lyrics", inline=False)
+        page1.add_field(name="Administration: ", value="General administration commands", inline=False)
+        page1.add_field(name="Maths: ", value="Various maths commands", inline=False)
+        page1.add_field(name="Fun: ", value="Fun and off-topic commands", inline=False)
+        page1.add_field(name="Misc: ", value="Bot information or commands without category", inline=False)
 
-        if re.search(r"\d", str(cog)):
-            cog = int(cog)
-            if cog > totalPages or cog < 1:
-                await ctx.send(f"Invalid page number: {cog} Please pick from {totalPages} pages or with no args.")
-                return
+        page2 = Page(title="Utility tools pt1", description = "Networking and hacking tools", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("networkingtools").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page2.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
+        for command in self.client.get_cog("shodan").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page2.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-            embed.set_footer(text=f"Page {cog} of {totalPages}")
+        page3 = Page(title="Utility tools pt2", description = "Website and cryptography tools", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("websites").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page3.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
+        for command in self.client.get_cog("encoding").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page3.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-            neededCogs = []
-            for i in range(4):
-                x = i + (int(cog) - 1) * 4
-                try:
-                    neededCogs.append(cogs[x])
-                except IndexError:
-                    pass
-            for cog in neededCogs:
-                commandList = ""
-                for command in self.client.get_cog(cog).walk_commands():
-                    if command.hidden:
-                        continue
-                    elif command.parent is not None:
-                        continue
-                    commandList += f"**{command.name}** - *{command.description}** -  Format:** {syntax(command)}*\n\n"
-                commandList += "\n"
+        page4 = Page(title="AI", description="Commands using AI", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("AI").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page4.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-                embed.add_field(name=cog, value=commandList, inline=False)
+        page5 = Page(title="Recourses", description="A list of programming and hacking recourses", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("recourses").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page5.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
+        for command in self.client.get_cog("googleapi").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page5.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-        elif re.search(r"a-zA-Z", str(cog)):
-            lowerCogs = [c.lower() for c in cogs]
-            if cog.lower() not in lowerCogs:
-                await ctx.send(f"Invalid arg: {cog}, please pick from page {totalPages}")
-                return
+        page6 = Page(title="Music", description="Music commands and song lyrics", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("music").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page6.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-            embed.set_footer(
-                text=f"Cog {(lowerCogs.index(cog.lower())+1)} of {len(lowerCogs)}. [] Optional, <> Required.")
+        page7 = Page(title="Administration", description="General administration commands", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("admin").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page7.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
+        for command in self.client.get_cog("reactroles").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page7.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-            helpText = ""
+        page8 = Page(title="Maths", description="Various maths commands", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("maths").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page8.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-            for command in self.client.get_cog(cogs[lowerCogs.index(cog.lower())]).walk_commands():
-                if command.hidden:
-                    continue
+        page9 = Page(title="Fun pt1", description="Fun and off-topic commands", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("fun").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page9.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
+        for command in self.client.get_cog("minecraft").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page9.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-                elif command.parent is None:
-                    continue
+        page10 = Page(title="Fun pt2", description="Fun and off-topic commands", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("reddit_commands").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page10.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
-                helpText += f"```{command.name}```\n**{command.description}**\n Format:** {syntax(command)}"
+        page11 = Page(title="Misc", description="Bot information or commands without catergory", inline=False, colour=discord.Colour.random())
+        for command in self.client.get_cog("misc").walk_commands():
+            if command.hidden:
+                continue
+            elif command.parent is not None:
+                continue
+            page11.add_field(name=f"**{command.name}**", value=f"{command.description} - Format: {syntax(command)}", inline=False)
 
+        pages = [page1, page2, page3, page4, page5, page6, page7, page8, page9, page10, page11]
+        menu.add_pages(pages)
+        menu.set_timeout(180)
+        menu.show_skip_buttons()
+        menu.show_page_numbers()
+
+        await menu.open()
+
+    @help.error
+    async def help_error(self, ctx, error):
+        if isinstance(error, commands.BotMissingPermissions):
+            await ctx.send("Bot is missing the manage messages permission")
         else:
-            await ctx.send(f"Invalid argument: `{cog}`\nPlease pick from {totalPages} pages.")
-            return
-        await ctx.send(embed=embed)
+            raise
+
+
+
 
 def setup(client):
     client.add_cog(help(client))
