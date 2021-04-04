55bb47158a8e361f53307e1a46268a9ec9bfea1b
Cinnamon1212 <65077960+Cinnamon1212@users.noreply.github.com>
2021-04-04 22:23:43 +0100

    Update reddit.py

--- lib/cogs/reddit.py
+++ lib/cogs/reddit.py
@@ -2,6 +2,7 @@ import discord
 import praw
 import random
 import os
+import json
 from discord.ext import commands
 from discord import Embed
 
@@ -13,61 +14,51 @@ reddit = praw.Reddit(client_id="mJfUbgRdBTcR6A",
                      user_agent="pythonbot")
 
 
+
 class reddit_commands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name="rule34", aliases=['nsfw', 'r34'], description="Random post from the rule34 subreddit")
-    async def rule34(self, ctx):
-        if ctx.channel.is_nsfw():
-            subreddit = reddit.subreddit("rule34")
-            all_subs = []
-
-            top = subreddit.top(limit=50)
-
-            for submission in top:
-                all_subs.append(submission)
-
-            random_sub = random.choice(all_subs)
-
-            name = random_sub.title
-            url = random_sub.url
-
-            embed = discord.Embed(title=name, color=discord.Color.gold())
-            embed.set_image(url=url)
-            embed.set_footer(text=f"Asked by {ctx.author.name}")
-
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send("Please use this command in an NSFW channel")
-
-    @commands.command(name="minecraft", aliases=['mc'], description="Random post from the minecraft subreddit")
-    async def minecraft(self, ctx):
-        subreddit = reddit.subreddit("minecraft")
+    @commands.command(name="codingmemes", description="Random coding meme from the coding meme subreddit", aliases=["codememe"])
+    async def codingmemes(self, ctx):
+        subreddit = reddit.subreddit('codingmemes')
         all_subs = []
-
         top = subreddit.top(limit=50)
 
         for submission in top:
             all_subs.append(submission)
-
         random_sub = random.choice(all_subs)
-
-        name = random_sub.title
-        url = random_sub.url
-
-        embed = discord.Embed(title=name, color=discord.Color.gold())
-        embed.set_image(url=url)
+        embed = discord.Embed(title=submission.name, colour=discord.Colour.random())
+        embed.set_image(submission.url)
         embed.set_footer(text=f"Asked by {ctx.author.name}")
         await ctx.send(embed=embed)
-
     @commands.command(name="searchreddit", aliases=['rsearch', 'redsearch'], description="Returns random post from a given subreddit")
-    async def searchreddit(self, ctx, usergivensub):
-        usergivensub = str(usergivensub)
-        subreddit = reddit.subreddit(usergivensub)
-        all_subs = []
-        if subreddit.over18:
-            if ctx.channel.is_nsfw():
+    async def searchreddit(self, ctx, *, usergivensub: str):
+        try:
+            subreddit = reddit.subreddit(usergivensub)
+            sub_exists = True
+        except NotFound:
+            await ctx.send(f"Unable to find {subreddit}")
+        if sub_exists == True:
+            all_subs = []
+            if subreddit.over18:
+                if ctx.channel.is_nsfw():
+                    top = subreddit.top(limit=50)
+
+                    for submission in top:
+                        all_subs.append(submission)
+                    random_sub = random.choice(all_subs)
+                    name = random_sub.title
+                    url = random_sub.url
+
+                    embed = discord.Embed(title=name, colour=discord.Colour.gold())
+                    embed.set_image(url=url)
+                    embed.set_footer(text=f"Asked by {ctx.author.name}")
+                    await ctx.send(embed=embed)
+
+                else:
+                    await ctx.send("Please use this command in an NSFW channel")
+            else:
                 top = subreddit.top(limit=50)
 
                 for submission in top:
@@ -76,28 +67,19 @@ class reddit_commands(commands.Cog):
                 random_sub = random.choice(all_subs)
                 name = random_sub.title
                 url = random_sub.url
-
                 embed = discord.Embed(title=name, color=discord.Color.gold())
                 embed.set_image(url=url)
                 embed.set_footer(text=f"Asked by {ctx.author.name}")
                 await ctx.send(embed=embed)
 
-            else:
-                await ctx.send("Please use this command in an NSFW channel")
+    @searchreddit.error
+    async def rsearch_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send("There was an error while fetching the subreddit")
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please enter the subreddit name!")
         else:
-            top = subreddit.top(limit=50)
-
-            for submission in top:
-                all_subs.append(submission)
-
-            random_sub = random.choice(all_subs)
-            name = random_sub.title
-            url = random_sub.url
-            embed = discord.Embed(title=name, color=discord.Color.gold())
-            embed.set_image(url=url)
-            embed.set_footer(text=f"Asked by {ctx.author.name}")
-            await ctx.send(embed=embed)
-
+            raise
 
 def setup(client):
     client.add_cog(reddit_commands(client))
