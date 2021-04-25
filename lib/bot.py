56a0fa3911013a9e53483f83c6c66cd8037decaa
Cinnamon1212 <65077960+Cinnamon1212@users.noreply.github.com>
2021-04-25 13:44:20 +0100

    Update bot.py

--- lib/bot.py
+++ lib/bot.py
@@ -1,7 +1,7 @@
 import discord, os, json
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
-client = commands.Bot(command_prefix='./', case_insensitive=True,
+client = commands.Bot(command_prefix='.//', case_insensitive=True,
                       intents=discord.Intents.all())
 
 with open('secrets.json', 'r') as secrets:
@@ -9,10 +9,13 @@ with open('secrets.json', 'r') as secrets:
 tokendata = json.loads(data)
 token = tokendata['token']
 
+
+# Owner commands
 @client.command()
 @commands.is_owner()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
+
 @client.command()
 @commands.is_owner()
 async def unload(ctx, extension):
@@ -20,8 +23,41 @@ async def unload(ctx, extension):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
-client.run(token, reconnect=True)
 
+@client.command()
+@commands.is_owner()
+async def ShowServers(ctx):
+    servers = client.guilds
+    names = []
+    for guild in servers:
+        names.append([guild.name, guild.member_count])
+    text = """
+----------------
+Server: members
+----------------
+"""
+    for name in names:
+        text += f"{name[0]}: {name[1]}\n"
+    await ctx.send(f"```{text}```")
+@client.command()
+@commands.is_owner()
+async def remote_shutdown(ctx):
+    await ctx.send("Shutting down!")
+    await client.close()
+
+@client.command()
+@commands.is_owner()
+async def changestatus(ctx, status, game):
+    if status == "online":
+        await client.change_presence(status=discord.Status.online, activity=discord.Game(game))
+    elif status == "idle":
+        await client.change_presence(status=discord.Status.idle, activity=discord.Game(game))
+    elif status == "dnd":
+        await client.change_presence(status=discord.Status.dnd, activity=discord.Game(game))
+    elif status == "invis":
+        await client.change_presence(status=discord.Status.invisible, activity=discord.Game(game))
+    else:
+        await ctx.send("Invalid status")
 
 async def shutdown(self):
     print("Closing connection to Discord...")
@@ -54,7 +90,7 @@ async def on_error(self, err, *args, **kwargs):
 
 async def on_command_error(self, ctx, exc):
     if isinstance(exc, CommandNotFound):
-        await ctx.send("No such command, please use ./help")
+        pass
     elif hasattr(exc, "original"):
         raise exc.original
     else:
@@ -74,3 +110,5 @@ async def process_commands(self, msg):
 async def on_message(self, msg):
     if not msg.author.bot:
         await self.process_commands(msg)
+
+client.run(token, reconnect=True)
