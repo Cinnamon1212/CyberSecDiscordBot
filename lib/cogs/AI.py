bf2eaebf313da526e48f661642ca536345d98786
Cinnamon1212 <65077960+Cinnamon1212@users.noreply.github.com>
2021-04-29 13:28:43 +0100

    Update AI.py

--- lib/cogs/AI.py
+++ lib/cogs/AI.py
@@ -1,4 +1,4 @@
-import discord, requests, random, sys, json, math, datetime, os
+import discord, requests, random, sys, json, math, datetime, os, magic
 from discord.ext import commands
 import cv2
 
@@ -17,14 +17,31 @@ class AI(commands.Cog):
 
     @commands.command(name="FaceDetection", description="attempts to find a face in an image", aliases=["facedetect", "detectfaces"])
     async def FaceDetection(self, ctx):
-        attachment_name = ctx.message.attachments[0].filename
-        attachment_url = ctx.message.attachments[0].url
-        r = requests.get(attachment_url)
-        with open(f"./AI/{ctx.author.id}_{attachment_name}", "wb") as f:
-            f.write(r.content)
-        path_to_image = f"./AI/{ctx.author.id}_{attachment_name}"
-        await facedetect_f(ctx, attachment_name, path_to_image)
-        await ctx.send(file=discord.File(path_to_image))
+        text = "Usage: ./FaceDetect // Attach a file"
+        if ctx.message.attachments:
+            attachment_name = ctx.message.attachments[0].filename
+            extension = attachment_name.split(".")[-1]
+            accepted_extensions = ["png", "jpeg", "jpg", "bmp"]
+            if "/" in attachment_name:
+                await ctx.send(f"```Filenames cannot contain /\'s \n{text}```")
+            elif extension not in accepted_extensions:
+                await ctx.send(f"```Unaccepted file extension\n{text}```")
+            else:
+                attachment_url = ctx.message.attachments[0].url
+                r = requests.get(attachment_url)
+                with open(f"./AI/{ctx.author.id}_{attachment_name}", "wb") as f:
+                    f.write(r.content)
+                path_to_image = f"./AI/{ctx.author.id}_{attachment_name}"
+                accepted_mimes = ["image/jpeg", "image/jpg", "image/png", "image/bmp"]
+                if magic.from_file(path_to_image, mime=True) in accepted_mimes:
+                    await facedetect_f(ctx, attachment_name, path_to_image)
+                    await ctx.send(file=discord.File(path_to_image))
+                else:
+                    await ctx.send(f"```Unsupported file type!\n{text}```")
+                    os.remove(path_to_image)
+        else:
+            await ctx.send(f"```Please attach an image\n{text}```")
+
 
     @commands.command(name="AIChat")
     async def AIChat(self, ctx, *, message):
