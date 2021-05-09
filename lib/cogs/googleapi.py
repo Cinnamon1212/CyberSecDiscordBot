6bf7f93f8fbdb029656efc7183415c65c0f7254d
Cinnamon1212 <65077960+Cinnamon1212@users.noreply.github.com>
2021-05-09 14:46:11 +0100

    Update googleapi.py

--- lib/cogs/googleapi.py
+++ lib/cogs/googleapi.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from discord.ext import commands
 from discord import Embed
 from aiohttp import request
-from googleapiclient.discovery import build
 
 with open('secrets.json', 'r') as secrets:
     data = secrets.read()
@@ -18,13 +17,13 @@ class googleapi(commands.Cog):
 
     @commands.command(name="exploitdb", description="Search exploitdb", aliases=["exDB"])
     async def exploitdb(self, ctx, *, query):
-        google = build('customsearch', 'v1', developerKey=apikey)
-        search_response = google.cse().list(
-            q=query,
-            cx="52b54907be70ff59c"
-        ).execute()
+        q=query,
+        cx="52b54907be70ff59c"
+        url = f"https://www.googleapis.com/customsearch/v1?key={apikey}&cx={cx}&q={q}&start=1"
+        async with request("GET", url) as response:
+            search_response = await response.json()
         embed = Embed(title=f"top 10 search results for {query}",
-                      colour=discord.Colour.random())
+                      colour=discord.Colour.red())
         embed.set_thumbnail(url="https://www.exploit-db.com/images/spider-white.png")
         for item in search_response['items']:
             embed.add_field(name=item['title'], value=f"{item['snippet']} [Link]({item['link']})", inline=False)
@@ -32,10 +31,11 @@ class googleapi(commands.Cog):
 
     @exploitdb.error
     async def exdb_error(self, ctx, error):
+        text = "Usage: ./exdb [query]"
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Please enter something to query")
+            await ctx.send(f"```Please enter something to query\n{text}```")
         elif isinstance(error, commands.CommandInvokeError):
-            await ctx.send("No search result!")
+            await ctx.send(f"```No search result!\n{text}```")
         else:
             raise
 
