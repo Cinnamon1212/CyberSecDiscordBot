58a468260c2fbd67b77f6b572b2d77fc021c9eb5
Cinnamon1212 <65077960+Cinnamon1212@users.noreply.github.com>
2021-06-18 13:37:12 +0100

    Update AI.py

--- lib/cogs/AI.py
+++ lib/cogs/AI.py
@@ -1,6 +1,12 @@
 import discord, requests, random, sys, json, math, datetime, os, magic, cv2
 from discord.ext import commands
 from discord import Embed
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow.keras.models import load_model
+
+wilfredOwenModel = load_model("./AI/wilfredowen.model")
+poetryText = open("./AI/wilfredowen.txt", "r").read().lower()
 
 async def facedetect_f(ctx, attachment_name, path_to_image):
     trained_face_data = cv2.CascadeClassifier('./AI/haarcascade_frontface.xml')
@@ -10,6 +16,33 @@ async def facedetect_f(ctx, attachment_name, path_to_image):
     for (x, y, w, h) in face_coords:
         cv2.rectangle(img, (x, y), (x+h, y+h), (random.randrange(256), random.randrange(256), random.randrange(256)), 2)
     cv2.imwrite(f"./AI/{ctx.author.id}_{attachment_name}", img)
+
+async def probGenerator(preds, temp):
+    # https://github.com/dgibbs2016/Shakespeare-in-LSTM/blob/c7e1399c92b794bbcacbd754b4e47c53682de313/lstm_text_generation.py#L62
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temp
+    expPreds = np.exp(preds)
+    preds = expPreds / np.sum(expPreds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+async def textGenerator(length, temperature, text, SEQ_LENGTH, characters, index_to_char, char_to_index):
+    start_index = random.randint(0, len(text) - SEQ_LENGTH - 1)
+    generated = ''
+    sentence = text[start_index: start_index + SEQ_LENGTH]
+    generated += sentence
+    for i in range(length):
+        x_predictions = np.zeros((1, SEQ_LENGTH, len(characters)))
+        for t, char in enumerate(sentence):
+            x_predictions[0, t, char_to_index[char]] = 1
+
+        predictions = wilfredOwenModel.predict(x_predictions, verbose=0)[0]
+        next_index = await probGenerator(predictions, temperature)
+        next_character = index_to_char[next_index]
+
+        generated += next_character
+        sentence = sentence[1:] + next_character
+    return generated
 
 class AI(commands.Cog):
     def __init__(self, client):
@@ -41,6 +74,29 @@ class AI(commands.Cog):
                     os.remove(path_to_image)
         else:
             await ctx.send(f"```Please attach an image\n{text}```")
+
+    @commands.command(name="wilfredowen", description="Use AI to generate a poem in the style of wilfred owen")
+    async def wilfredowen(self, ctx, charCount: int = 300):
+        if charCount > 500:
+            await ctx.send(f"```You can only use a maximum of 500 characters```")
+        else:
+            SeqLen = 40
+            StepSize = 3
+            charSet = sorted(set(poetryText))
+            charToIndex = dict((c, i) for i, c in enumerate(charSet))
+            indexToChar = dict((i, c) for i, c in enumerate(charSet))
+            generatedText = await textGenerator(300, 1.0, poetryText, SeqLen, charSet, indexToChar, charToIndex)
+            await ctx.send(f"```{generatedText}```")
+
+    @wilfredowen.error
+    async def wilfredowen_error(self, ctx, error):
+        text = "Usage: ./wilfredowen (character count)"
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(f"```{text}```")
+        else:
+            await ctx.send(f"```An unknown error has occured\n{text}```")
+            raise error
+
 
 def setup(client):
     client.add_cog(AI(client))
