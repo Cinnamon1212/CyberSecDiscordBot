5c46aca615cdda02e81c116216d414f888a7c0b2
Cinnamon1212 <65077960+Cinnamon1212@users.noreply.github.com>
2021-06-20 19:26:28 +0100

    Update bot.py

--- lib/bot.py
+++ lib/bot.py
@@ -4,7 +4,7 @@ from discord.ext.commands import CommandNotFound
 intents = discord.Intents.default()
 
 
-client = commands.Bot(command_prefix='.//', case_insensitive=True,
+client = commands.Bot(command_prefix='./', case_insensitive=True,
                       intents=intents)
 
 with open('secrets.json', 'r') as secrets:
@@ -42,6 +42,7 @@ Server: members
     for name in names:
         text += f"{name[0]}: {name[1]}\n"
     await ctx.send(f"```{text}```")
+
 @client.command()
 @commands.is_owner()
 async def remote_shutdown(ctx):
@@ -61,57 +62,5 @@ async def changestatus(ctx, status, *, game):
         await client.change_presence(status=discord.Status.invisible, activity=discord.Game(game))
     else:
         await ctx.send("Invalid status")
-
-async def shutdown(self):
-    print("Closing connection to Discord...")
-
-    await super().close()
-
-
-async def close(self):
-    print("Closing on keyboard interrupt...")
-    await self.shutdown()
-
-
-async def on_connect(self):
-    print(f" Connected to Discord (latency: {self.latency*1000:,.0f} ms).")
-
-
-async def on_resumed(self):
-    print("Bot resumed.")
-
-
-async def on_disconnect(self):
-    print("Bot disconnected.")
-
-
-async def on_error(self, err, *args, **kwargs):
-    if err == "on_command_error":
-        await args[0].send("Something went wrong")
-    raise
-
-
-async def on_command_error(self, ctx, exc):
-    if isinstance(exc, CommandNotFound):
-        pass
-    elif hasattr(exc, "original"):
-        raise exc.original
-    else:
-        raise exc
-
-
-async def on_ready(self):
-    self.client_id = (await self.application_info()).id
-    print("Bot ready.")
-
-async def process_commands(self, msg):
-    ctx = await self.get_context(msg, cls=commands.Context)
-
-    if ctx.command is not None:
-        await self.invoke(ctx)
-
-async def on_message(self, msg):
-    if not msg.author.bot:
-        await self.process_commands(msg)
 
 client.run(token, reconnect=True)
